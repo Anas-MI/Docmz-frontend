ed8861e73518aa176f9af1e7aa23b167f441f8d2
Anas-MI <54795271+Anas-MI@users.noreply.github.com>
2019-10-11 10:59:29 +0530

    added fields

---
+++ src/components/Fields/ValidateFields.js
@@ -0,0 +1,26 @@
+export const validateEmail = (value) => {
+	let errors;
+
+	if (!value) {
+		errors = 'Required!';
+	} else if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+		errors = 'Invalid email address!';
+	}
+
+	return errors;
+};
+
+export const isRequired = (value) => (!value ? 'Required!' : '');
+
+export const validatePassword = (value) => {
+	let errors;
+
+	if (!value) {
+		errors = 'Password is Required!';
+	} else if (value.length <= 3) {
+		errors = 'Password too short!';
+	} else if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$/.test(value)) {
+		errors = 'Password seems quite weak!';
+  }
+  return errors;
+};
