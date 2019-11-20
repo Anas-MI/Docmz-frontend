6ffa465590e8cf7541672bf434f6c64502c77cde
Anas-MI <54795271+Anas-MI@users.noreply.github.com>
2019-11-20 18:51:16 +0530

    demographic css change

--- src/pages/Patient/Settings/Demographic/Demographic.js
+++ src/pages/Patient/Settings/Demographic/Demographic.js
@@ -150,23 +150,25 @@ class Demographic extends Component {
                 <Layout className="layout">
                     <Navbar />
 
-                    <Content style={{ padding: '0 50px', marginTop: 64 }}>
+                    <Content style={{ padding: '0 50px', marginTop: 64 }} className="custom-home-content-ap">
 
-                        <div style={{ background: '#fff', padding: 24, minHeight: 380 }} className="doctor-header">
-                            <Uppermsg />
-                            <header className="App-header">
-                                <Search />
-                            </header>
-                            <Content style={{ padding: '0 50px' }}>
-                                <Layout style={{ padding: '24px 0', background: '#fff' }}>
+<div style={{ background: 'transparent', padding: 24, minHeight: 380 }} className="doctor-header">
+    <Uppermsg />
+    <header className="App-header">
+        <Search />
+    </header>
+    <Content style={{ padding: '0', marginTop : '30px' }}>
+        <Layout style={{ padding: '24px 0'
+        //  background: '#fff'
+          }}>
 
                                     <Sidebar />
-                                    <Content style={{ padding: '0 24px', minHeight: 280 }}>
+                                    <Content style={{  minHeight: 280 }} className="custom-home-content-inner-ap-patient">
 
                                         <Layout>
 
                                             <Content className="patient-profile-content">
-                                                <p>Demographic Info (optional)</p>
+                                                <p className="profile-header-custom-patient-ap"><strong>Demographic Info (optional)</strong></p>
                                                 <Divider />
 
                                                 <Form onSubmit={this.handleSubmit}>
