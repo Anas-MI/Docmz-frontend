efdbcb24f8f3481977bc160be6a49eeb0d8abc0b
thirdessential <thirdessential@gmail.com>
2019-11-23 18:40:41 +0530

    policy and consent form popup implement

--- src/components/appointment/AppointmentForm.js
+++ src/components/appointment/AppointmentForm.js
@@ -11,14 +11,22 @@ import {
   Steps,
   List,
   Typography,
-  Divider
+  Divider,
+  Radio,
+  Tooltip,
+  Cascader,
+  Select,
+  Checkbox,
+  AutoComplete,
 } from "antd";
+import {  Field } from "formik";
 import FormStep1 from "./FormStep1";
 import { patientCardList } from "../../services/api/patient";
 import PaymentCard from "../../components/payment/PaymentCard";
 import AppointmentPayReview from "./AppointmentPayReview";
 import AppointmentDoctor from "./AppointmentDoctor";
 import './newappointmentap.css'
+
 const stepStyle = {
   marginBottom: 37,
   boxShadow: "0px -1px 0 0 #e8e8e8 inset"
@@ -35,6 +43,8 @@ const getCardData = () => {
       console.log({ err });
     });
 };
+// class AppointmentForm extends React.Component{ 
+//   render () {
 export default function AppointmentForm() {
   const FormOne = Form.create({ name: "appointment_form_one" })(FormStep1);
   const FormThree = Form.create({ name: "appointment_form_three" })(
@@ -53,12 +63,16 @@ export default function AppointmentForm() {
     });
   };
 
+
+
   const cardDetailsWithNextStep = data => {
     setCardDetails(data);
     setCurrentStep(2);
   };
+
   const card = getCardData();
   const [currentStep, setCurrentStep] = useState(0);
+  const [value, setValue] = useState("");
   const [selectedCard, setSelectedCard] = useState(null);
   const [firstStapForm, setFirstStapForm] = useState("");
   const [cardDetails, setCardDetails] = useState("");
@@ -73,9 +87,29 @@ export default function AppointmentForm() {
   };
   const setSavedCardData = data => {
     setCardDetails(data);
+    setCurrentStep(3);
+    console.log('currentstep', data.id)
+
+  };
+  const phonesubmit = e => {
+    // setCardDetails(data);
     setCurrentStep(2);
   };
-
+  const radioStyle = {
+    display: 'block',
+    height: '30px',
+    lineHeight: '30px',
+  };
+  // const prefixSelector = getFieldDecorator('prefix', {
+  //   initialValue: '86',
+  // })(
+  //   <Select style={{ width: 70 }}>
+  //     <Option value="86">+86</Option>
+  //     <Option value="87">+87</Option>
+  //   </Select>,
+  // );
+  // const { Option } = Select
+  // const { getFieldDecorator } = this.props.form;
   return (
     <div className="c-appointment-form">
       <div className="c-appointment-form__header ap-appointment-form-custom">
@@ -92,48 +126,114 @@ export default function AppointmentForm() {
           />
           <Step
             status={currentStep < 2 ? "finish" : "process"}
-            title="Payment"
+            title="Visit Type"
           />
           <Step
             status={currentStep < 3 ? "finish" : "process"}
+            title="Payment"
+          />
+          <Step
+            status={currentStep < 4 ? "finish" : "process"}
             title="Review and Pay"
           />
+
         </Steps>
       </div>
 
-      <div className="below-para-steps-custom-ap">
+      {/* <div className="below-para-steps-custom-ap">
         <p>We just need a few details to make your appointment smooth</p>
         <Divider />
-      </div>
+      </div> */}
       {currentStep === 0 && (
-        <Row type="flex">
-          <Col span={12}>
-            <div className="c-appointment-form__steps">
-              <div className="c-appointment-form__step custom-ap-details-form-label">
-                <FormOne ref={formOne} />
+        <div>
+          <Row>
+            <Col span={24}>
+              <p className="below-para-steps-custom-ap">We just need a few details to make your appointment smooth</p>
+              <Divider />
+            </Col>
+          </Row>
+          <Row type="flex">
+
+            <Col span={12}>
+              <div className="c-appointment-form__steps">
+                <div className="c-appointment-form__step custom-ap-details-form-label">
+                  <FormOne ref={formOne} />
+                </div>
+                <Button
+                  type="primary"
+                  className="ap-appointment-details-btn"
+                  onClick={() => {
+                    if (typeof formOne.current.submit === "function") {
+                      formOne.current.submit(handleSubmit);
+                    }
+                  }}
+                >
+                  Next
+              </Button>
               </div>
+            </Col>
+            <Col span={12}>
+              <div className="c-appointment-form__doctor-wrapper">
+                <AppointmentDoctor doctor={{ name: "doctor name" }} time="1040 - 1240" />
+              </div>
+            </Col>
+          </Row>
+        </div>
+      )}
+      {currentStep === 1 && (
+        <div className="second-step-custom-ap">
+          <Row>
+            <Col span={24}>
+              <h2 >John, Let's get you taken care of</h2>
+
+            </Col>
+          </Row>
+          <Row >
+            <Col span={24} style={{ textAlign: 'center' }}>
+              <p className="visit-type-para-ap">Which type of visit would you like?</p>
+            </Col>
+
+            <Col xs={{ span: 11, offset: 1 }} lg={{ span: 5, offset: 7 }}>
+
+              <Icon type="phone" className="second-step-custom-ap__icons" />
+
+            </Col >
+            <Col xs={{ span: 11, offset: 1 }} lg={{ span: 10, offset: 2 }}>
+              <Icon type="mobile" className="second-step-custom-ap__icons"/>
+            </Col>
+            <Col span={24} align="middle">
+              <p className="video-phone-para"><a href="#">Video vs Phone visits. Learn how they work</a></p>
+            </Col>
+           </Row>
+           <Row >
+           
+           
+            <Divider />
+            <Col span={24} style={{ textAlign: 'center' }}>
+              <p className="visit-type-para-ap">What's the best number to reach you during your visit?</p>
+            </Col>
+            <Col span={12} >
+              <p>8562025363</p>
+             
+              
+            </Col>
+            <Col span={12}>
+              <p className="second-step-custom-ap_why-para_ap">Why do we need this?</p>
+            </Col>
+
+          </Row>
+          <Row>
+            <Col span={24}>
               <Button
                 type="primary"
                 className="ap-appointment-details-btn"
-                onClick={() => {
-                  if (typeof formOne.current.submit === "function") {
-                    formOne.current.submit(handleSubmit);
-                  }
-                }}
+                onClick={() => phonesubmit()}
               >
                 Next
               </Button>
-            </div>
-          </Col>
-          <Col span={12}>
-            <div className="c-appointment-form__doctor-wrapper">
-              <AppointmentDoctor doctor={      { name: "doctor name" }} time="1040 - 1240" />
-            </div>
-          </Col>
-        </Row>
-      )}
-      {currentStep === 1 && (
-        <Row type="flex">
+            </Col>
+          </Row>
+          {/* <Row type="flex">
           <Col span={24}>
             <div className="c-appointment-form__steps">
               <div className="c-appointment-form__step">
@@ -156,19 +256,30 @@ export default function AppointmentForm() {
                       }}
                     />
                   ) : (
-                      <div>
+                      <div className="custom-card-list-ap">
+                      
+                      
                         <List
+                        className="custom-card-list-data-ap"
                           bordered
                           dataSource={cards}
                           renderItem={item => (
-                            <List.Item onClick={() => setSavedCardData(item)}>
-                              <p className="c-appointment-form__card-number">
-                                xxxx xxxx xxxx {item.last4}
+                       
+                            
 
+                       
+                            <List.Item onClick={() => setSavedCardData(item)}>
+                                
+                               <p className="c-appointment-form__card-number">
+                          
+                                xxxx xxxx xxxx {item.last4}  
+                          <span className="middle-content-date">{item.exp_month}/{item.exp_year}</span>
                                 <span className="c-appointment-form__card-icon">
-                                  <Icon type="credit-card" />
+                                  <span style={{fontSize : '18px'}}>{item.brand}</span> - <Icon type="credit-card" />
                                 </span>
-                              </p>
+                              
+                              </p> 
+                             
                             </List.Item>
                           )}
                         />
@@ -186,18 +297,121 @@ export default function AppointmentForm() {
               </div>
             </div>
           </Col>
-        </Row>
+        </Row> */}
+        </div>
       )}
       {currentStep === 2 && (
-        <Row type="flex">
-          <Col span={24}>
-            <FormThree
-              firstFormData={firstStapForm}
-              cardDetails={cardDetails}
-            />
-          </Col>
-        </Row>
+        <div>
+          <Row>
+            <Col span={24}>
+              <p className="below-para-steps-custom-ap">We will not share your payment details</p>
+              <Divider />
+            </Col>
+          </Row>
+          <Row type="flex">
+            <Col span={24}>
+              <div className="c-appointment-form__steps">
+                <div className="c-appointment-form__step">
+                  <div className="">
+                    {addCard ? (
+                      <PaymentCard
+                        cvvOnCard={""}
+                        expDateOnCard={""}
+                        numberOnCard={""}
+                        nameOnCard={""}
+                        cardResponse={response => {
+                          console.log("response", { response });
+                        }}
+                        transactionData={e => {
+                          cardDetailsWithNextStep(e);
+                        }}
+                        saveOptional={true}
+                        backButton={() => {
+                          toggleCard();
+                        }}
+                      />
+                    ) : (
+                        <div className="custom-card-list-ap">
+
+                          {/* </Radio.Group> */}
+                          <List
+                            className="custom-card-list-data-ap"
+                            bordered
+                            dataSource={cards}
+                            renderItem={item => (
+
+
+
+
+                              <List.Item onClick={() => setSavedCardData(item)}>
+                                {/* <Radio.Group 
+                               
+                                 >
+                                    <Radio value={item.id}>
+                                    <span>
+                                      xxxx xxxx xxxx {item.last4} 
+                                      
+                                       </span>
+                                       <span>
+                                       {item.exp_month}/{item.exp_year} 
+                                      
+                                       </span>
+                                       <span>
+                                       {item.brand} - <Icon type="credit-card" />
+                                      
+                                       </span> */}
+                                {/* <Radio value={item.id}> */}
+                                <p className="c-appointment-form__card-number">
+
+                                  xxxx xxxx xxxx {item.last4}
+                                  <span className="middle-content-date">{item.exp_month}/{item.exp_year}</span>
+                                  <span className="c-appointment-form__card-icon">
+                                    <span style={{ fontSize: '18px' }}>{item.brand}</span> - <Icon type="credit-card" />
+                                  </span>
+
+                                </p>
+                                {/* </Radio> */}
+
+                                {/* </Radio> */}
+
+                                {/* </Radio.Group> */}
+                              </List.Item>
+                            )}
+                          />
+                          <Button
+                            className="c-appointment-form__card-tgl-btn"
+                            onClick={() => {
+                              toggleCard();
+                            }}
+                          >
+                            Add New <Icon type="plus" />
+                          </Button>
+                        </div>
+                      )}
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
+      )}
+      {currentStep === 3 && (
+        <div>
+        
+          <Row type="flex">
+            <Col span={24}>
+              <FormThree
+                firstFormData={firstStapForm}
+                cardDetails={cardDetails}
+              />
+            </Col>
+          </Row>
+        </div>
       )}
     </div>
   );
 }
+// }
+
+// export default Form.create()(AppointmentForm)
+
--- src/components/appointment/AppointmentPayReview.js
+++ src/components/appointment/AppointmentPayReview.js
@@ -1,6 +1,44 @@
 import React from "react";
-import { Form, Icon, Input, Button, Row, Col, Divider } from "antd";
+import { Form, Icon, Input, Button, Row, Col, Divider, Checkbox,Modal } from "antd";
+import Firstmodel from "./Firstmodel";
+import Secondmodal from "./SecondModal";
 export default class AppointmentPayReview extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      visible: false,
+      secondvisible : false
+    }
+  }
+  showModal2 = () => {
+    this.setState({
+      secondvisible: true,
+     
+    });
+  }
+  showModal = () => {
+    this.setState({
+      visible: true,
+     
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+      secondvisible : false
+      
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+      secondvisible : false
+    });
+  };
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -15,22 +53,112 @@ export default class AppointmentPayReview extends React.Component {
   };
 
   render() {
+    const { getFieldDecorator } = this.props.form;
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 8,
+        },
+      },
+    };
     const { firstFormData, cardDetails } = this.props;
     return (
-      <Row type="flex" justify="space-around" align="middle">
-        <Col span={24} className="left-side-content-review-ap">
-          <h3>Your appointment will be schedule from <span>10:00 AM - 10:30 AM</span></h3>
-          <p>Appointment cost : <b>$30.00</b> </p>
-          <Divider/>
-         <center>
-         <Button type="info">Confirm Booking</Button>
-         </center> 
+      <div className="review-custom-section-ap">
+        <Row>
+          <Col span={12}>
+            <h2>Appointment Details</h2>
+            
+            <div classame="review-custom-section-ap__visit-details">
+            <p><strong>Consultation Method : </strong>Video</p>
+                <p><strong>Reason for visit :</strong> Toothache</p>
+                 <p><strong>Duration : </strong>6 Months</p> 
+                <p><strong>Consultation Cost : </strong>$40.00</p>
+              </div>
+          </Col>
+
+          <Col span={12}>
+          <h2>Payment Method</h2>
+            
+           
+            <p>Visa ending in : 4242</p>
+
+            <h2>Insurance</h2>
+            
+           
+            <p>No Insurance Addedd</p>
+            <h2>Primary Care Physician</h2>
+            
+           
+            {/* <p>No Data Yet.</p> */}
+                
+             
+          </Col>
+        </Row>
+        <Divider />
+        <Row>
+          <div className="review-custom-section-ap__checkbox-ap">
+          <Col span={24}>
+          <Form.Item>
+          {getFieldDecorator('agreement', {
+            valuePropName: 'checked',
+          })(
+            <Checkbox>
+             I certify that I have read and accept the terms of <span onClick={this.showModal} style={{color : '#82bbe9'}}>Doc Mz's Informed Consent</span>.
+            </Checkbox>,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('policy', {
+            valuePropName: 'checked',
+          })(
+            <Checkbox>
+            I have read <span onClick={this.showModal2} style={{color : '#82bbe9'}}>Doc Mz's Informed Consent</span> and I acknowledge that I have the ability to print a hard copy of Privacy Policy for my records.
+            </Checkbox>,
+          )}
+        </Form.Item>
+
+        <Modal
+          title="Informed Consent for DocMz"
+          width={1024}
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+         <Firstmodel />
+        </Modal>
+
+        <Modal
+          title="Privacy and policy terms of DocMz"
+          width={1024}
+          visible={this.state.secondvisible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+         <Secondmodal />
+        </Modal>
+            </Col>
+            </div>
+        </Row>
+      </div>
+      // <Row type="flex" justify="space-around" align="middle">
+      //   <Col span={24} className="left-side-content-review-ap">
+      //     <h3>Your appointment will be schedule for <strong>Toothache</strong> on <strong>2<sup>nd</sup> December 2020</strong> at <span>10:00 AM - 10:30 AM</span></h3>
+      //     <p>Consultation cost : <b>$30.00</b> </p>
+      //     <Divider/>
+      //    <center>
+      //    <Button type="info">Confirm Booking</Button>
+      //    </center> 
           
             
-        </Col>
+      //   </Col>
 
        
-      </Row>
+      // </Row>
     );
   }
 }
